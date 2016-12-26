dfbb12e0624d8157a23667ee197c1771ee15152f
nipa <nipa@codefx.org>
2016-12-26 23:40:50 +0100

    Use long forms of command line flags

--- compile.sh
+++ compile.sh
@@ -12,54 +12,54 @@ echo " > creating monitor.observer"
 javac9 \
 	-d classes/monitor.observer \
 	$(find monitor.observer -name '*.java')
-jar9 -c \
+jar9 --create \
 	--file mods/monitor.observer.jar \
 	-C classes/monitor.observer .
 
 echo " > creating monitor.observer.alpha"
 javac9 \
-	-p mods \
+	--module-path mods \
 	-d classes/monitor.observer.alpha \
 	$(find monitor.observer.alpha -name '*.java')
-jar9 -c \
+jar9 --create \
 	--file mods/monitor.observer.alpha.jar \
 	-C classes/monitor.observer.alpha .
 
 
 echo " > creating monitor.observer.beta"
 javac9 \
-	-p mods \
+	--module-path mods \
 	-d classes/monitor.observer.beta \
 	$(find monitor.observer.beta -name '*.java')
-jar9 -c \
+jar9 --create \
 	--file mods/monitor.observer.beta.jar \
 	-C classes/monitor.observer.beta .
 
 
 echo " > creating monitor.statistics"
 javac9 \
-	-p mods \
+	--module-path mods \
 	-d classes/monitor.statistics \
 	$(find monitor.statistics -name '*.java')
-jar9 -c \
+jar9 --create \
 	--file mods/monitor.statistics.jar \
 	-C classes/monitor.statistics .
 
 echo " > creating monitor.persistence"
 javac9 \
-	-p mods \
+	--module-path mods \
 	-d classes/monitor.persistence \
 	$(find monitor.persistence -name '*.java')
-jar9 -c \
+jar9 --create \
 	--file mods/monitor.persistence.jar \
 	-C classes/monitor.persistence .
 
 echo " > creating monitor"
 javac9 \
-	-p mods \
+	--module-path mods \
 	-d classes/monitor \
 	$(find monitor -name '*.java')
-jar9 -c \
+jar9 --create \
 	--file mods/monitor.jar \
 	--main-class monitor.Monitor \
 	-C classes/monitor .
