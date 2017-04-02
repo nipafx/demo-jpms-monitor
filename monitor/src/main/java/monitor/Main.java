1881dab259786927bde2f21e67c901b45195529f
nipa <nipa@codefx.org>
2017-04-02 15:37:42 +0200

    Log on missing observers instead of throwing an exception

--- monitor/src/main/java/monitor/Main.java
+++ monitor/src/main/java/monitor/Main.java
@@ -9,6 +9,7 @@ import monitor.statistics.Statistician;
 import monitor.statistics.Statistics;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -38,6 +39,7 @@ public class Main {
 	private static Monitor createMonitor() {
 		List<ServiceObserver> observers = Stream.of("alpha-1", "alpha-2", "alpha-3", "beta-1")
 				.map(Main::createObserver)
+				.flatMap(Optional::stream)
 				.collect(toList());
 		Statistician statistician = new Statistician();
 		StatisticsRepository repository = new StatisticsRepository();
@@ -46,10 +48,13 @@ public class Main {
 		return new Monitor(observers, statistician, repository, initialStatistics);
 	}
 
-	private static ServiceObserver createObserver(String serviceName) {
+	private static Optional<ServiceObserver> createObserver(String serviceName) {
 		return AlphaServiceObserver.createIfAlphaService(serviceName)
 				.or(() -> BetaServiceObserver.createIfBetaService(serviceName))
-				.orElseThrow(IllegalArgumentException::new);
+				.or(() -> {
+					System.out.printf("No observer for %s found.%n", serviceName);
+					return Optional.empty();
+				});
 	}
 
 }
