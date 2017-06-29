a5a12bfe2aed9585b4688b47d5d957ddc2af22e6
Nicolai Parlog <nipa@codefx.org>
2017-06-29 11:19:49 +0200

    [BREAK] Missing transitive dependency

--- monitor.statistics/src/main/java/module-info.java
+++ monitor.statistics/src/main/java/module-info.java
@@ -1,4 +1,9 @@
 module monitor.statistics {
 	requires monitor.observer;
+//	this requirements is useless; it is only there to demonstrate
+//	that a missing transitive dependency causes compile errors for
+//	dependant projects (in this case monitor.rest)
+	requires transitive monitor.observer.alpha;
+
 	exports monitor.statistics;
 }
